bccbc292c9ee4f27dea0934aa4e7bb24caa359dc
Alan <alandsantos54@gmail.com>
2025-02-20 23:37:09 -0300

    atualização 1

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,9 @@
 import { Router } from 'express';
-import { v4 } from 'uuid';
+import UserController from './app/controllers/UserController';
 
-import User from './app/models/User.js';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-   const user = await User.create({
-    id: v4(),
-    name: 'Paulo',
-    email: 'paulo12354@email.com',
-    password_hash: 'sdbdnjdnndncc',
-   });
-
-   return res.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
