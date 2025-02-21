59c72dae171cd1ce6e82ce374ea7d06908d8c8a8
Alan <alandsantos54@gmail.com>
2025-02-21 16:04:31 -0300

    aula 18

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -20,8 +20,11 @@ class User extends Model{
                 user.password_hash = await bcrypt.hash(user.password, 10);
             }
         })
-        
+
         return this;
+    }
+    async checkPassword(password) {
+     return bcrypt.compare(password, this.password_hash);
     }
 
 }
